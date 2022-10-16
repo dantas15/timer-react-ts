73cbb8a8f11bcc203841bcbb5567ade8115f78d1
Gustavo Galote <gusgalot3g@gmail.com>
2022-10-16 09:36:12 -0300

    feat(theme): setup global themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from 'styled-components';
 
 import { Button } from './components/Button';
+import { GlobalStyles } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Button>Test</Button>
+
+      <GlobalStyles />
     </ThemeProvider>
   );
 }
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -20,7 +20,10 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-  background-image: ${(props) => props.theme.colors.primary};
-  color: ${(props) => props.theme.colors};
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-300']};
 `;
